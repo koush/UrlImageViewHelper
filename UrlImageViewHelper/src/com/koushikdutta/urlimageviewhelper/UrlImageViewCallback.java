6c7b2bb063502d05315d448ea7b586694041460f
Koushik Dutta <koushd@gmail.com>
2012-05-14 20:32:23 -0700

    missing files

---
+++ UrlImageViewHelper/src/com/koushikdutta/urlimageviewhelper/UrlImageViewCallback.java
@@ -0,0 +1,8 @@
+package com.koushikdutta.urlimageviewhelper;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+public interface UrlImageViewCallback {
+    void onLoaded(ImageView imageView, Drawable loadedDrawable, String url, boolean loadedFromCache);
+}
---
+++ UrlImageViewHelperSample/gen/com/koushikdutta/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.koushikdutta.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
