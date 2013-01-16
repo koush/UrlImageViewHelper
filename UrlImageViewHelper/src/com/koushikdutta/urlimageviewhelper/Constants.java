84ffd261b449dea8e2a59afc49d068e39c672939
Koushik Dutta <koushd@gmail.com>
2013-01-15 23:47:13 -0800

    disable logging

--- UrlImageViewHelper/src/com/koushikdutta/urlimageviewhelper/Constants.java
+++ UrlImageViewHelper/src/com/koushikdutta/urlimageviewhelper/Constants.java
@@ -4,7 +4,7 @@ public interface Constants {
 
     public static final String LOGTAG = "UrlImageViewHelper";
 
-    public static final boolean LOG_ENABLED = true; //set to True to enable verbose logging
+    public static final boolean LOG_ENABLED = false; //set to True to enable verbose logging
 
     //set here and not in Build to maintain proper backwards compatibility
     public static final int HONEYCOMB = 11;
