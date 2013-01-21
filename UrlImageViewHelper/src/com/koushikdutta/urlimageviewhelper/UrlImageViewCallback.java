2d96958e8f3196de98c56eb3d46c991d4e8874de
Koushik Dutta <koushd@gmail.com>
2013-01-21 15:56:16 -0800

    more comments about callbacks

--- UrlImageViewHelper/src/com/koushikdutta/urlimageviewhelper/UrlImageViewCallback.java
+++ UrlImageViewHelper/src/com/koushikdutta/urlimageviewhelper/UrlImageViewCallback.java
@@ -6,6 +6,13 @@ import android.widget.ImageView;
 /**
  * Callback that is invoked with a success/failure after attempting to
  * load a drawable from an url.
+ * Note: If an ImageView has multiple setUrlDrawable calls made on it, only the last callback
+ * will be invoked. This scenario arises when using ListViews which recycle their views.
+ * This is done to prevent callbacks from being erroneosly invoked on ImageViews that are no
+ * longer interested in the url that was loaded.
+ * To guarantee a callback is invoked, one can do the following:
+ * First call loadUrlDrawable (with a callback), and then setUrlDrawable. Both loads just get queued into the same request,
+ * so you don't need to worry about that being inefficient or that it is making two network calls.
  * @author koush
  *
  */
