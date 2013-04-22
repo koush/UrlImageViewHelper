2f828760a8fa690eba3afb7832b054ae66ef143b
Koushik Dutta <koushd@gmail.com>
2013-04-22 16:42:33 -0700

    switch to a grid implementation that shows off scale animation in

--- UrlImageViewHelperSample/src/com/koushikdutta/urlimageviewhelper/sample/UrlImageViewHelperSample.java
+++ UrlImageViewHelperSample/src/com/koushikdutta/urlimageviewhelper/sample/UrlImageViewHelperSample.java
@@ -15,18 +15,25 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.DataSetObserver;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.ScaleAnimation;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.koushikdutta.urlimageviewhelper.UrlImageViewCallback;
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
 public class UrlImageViewHelperSample extends Activity {
@@ -45,8 +52,71 @@ public class UrlImageViewHelperSample extends Activity {
         return new String(buff.toByteArray());
     }
     
-    private GridView mListView;
+    private ListView mListView;
     private MyAdapter mAdapter;
+
+    private class Row extends ArrayList {
+        
+    }
+    
+    private class MyGridAdapter extends BaseAdapter {
+        public MyGridAdapter(Adapter adapter) {
+            mAdapter = adapter;
+            mAdapter.registerDataSetObserver(new DataSetObserver() {
+                @Override
+                public void onChanged() {
+                    super.onChanged();
+                    notifyDataSetChanged();
+                }
+                @Override
+                public void onInvalidated() {
+                    super.onInvalidated();
+                    notifyDataSetInvalidated();
+                }
+            });
+        }
+        Adapter mAdapter;
+        
+        @Override
+        public int getCount() {
+            return (int)Math.ceil((double)mAdapter.getCount() / 4d);
+        }
+
+        @Override
+        public Row getItem(int position) {
+            Row row = new Row();
+            for (int i = position * 4; i < 4; i++) {
+                if (mAdapter.getCount() < i)
+                    row.add(mAdapter.getItem(i));
+                else
+                    row.add(null);
+            }
+            return row;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.row, null);
+            LinearLayout row = (LinearLayout)convertView;
+            LinearLayout l = (LinearLayout)row.getChildAt(0);
+            for (int child = 0; child < 4; child++) {
+                int i = position * 4 + child;
+                LinearLayout c = (LinearLayout)l.getChildAt(child);
+                c.removeAllViews();
+                if (i < mAdapter.getCount()) {
+                    c.addView(mAdapter.getView(i, null, null));
+                }
+            }
+            
+            return convertView;
+        }
+        
+    }
     
     private class MyAdapter extends ArrayAdapter<String> {
 
@@ -57,15 +127,25 @@ public class UrlImageViewHelperSample extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView iv;
-//            if (convertView == null)
-                convertView = iv = (ImageView)getLayoutInflater().inflate(R.layout.image, null);
-//            else
-//                iv = (ImageView)convertView;
+            if (convertView == null)
+                convertView = getLayoutInflater().inflate(R.layout.image, null);
+
+            iv = (ImageView)convertView.findViewById(R.id.image);
             
+            iv.setAnimation(null);
             // yep, that's it. it handles the downloading and showing an interstitial image automagically.
-            UrlImageViewHelper.setUrlDrawable(iv, getItem(position), R.drawable.loading);
-            
-            return iv;
+            UrlImageViewHelper.setUrlDrawable(iv, getItem(position), R.drawable.transparent, new UrlImageViewCallback() {
+                @Override
+                public void onLoaded(ImageView imageView, Bitmap loadedBitmap, String url, boolean loadedFromCache) {
+                    if (!loadedFromCache) {
+                        ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1, ScaleAnimation.RELATIVE_TO_SELF, .5f, ScaleAnimation.RELATIVE_TO_SELF, .5f);
+                        scale.setDuration(140);
+                        imageView.startAnimation(scale);
+                    }
+                }
+            });
+
+            return convertView;
         }
     }
 
@@ -79,9 +159,10 @@ public class UrlImageViewHelperSample extends Activity {
         final Button search = (Button)findViewById(R.id.search);
         final EditText searchText = (EditText)findViewById(R.id.search_text);
         
-        mListView = (GridView)findViewById(R.id.results);
+        mListView = (ListView)findViewById(R.id.results);
         mAdapter = new MyAdapter(this);
-        mListView.setAdapter(mAdapter);
+        MyGridAdapter a = new MyGridAdapter(mAdapter);
+        mListView.setAdapter(a);
         
         search.setOnClickListener(new OnClickListener() {
             @Override
@@ -101,33 +182,32 @@ public class UrlImageViewHelperSample extends Activity {
                             
                             // do a google image search, get the ~10 paginated results
                             int start = 0;
-                            while (start < 10) {
+                            final ArrayList<String> urls = new ArrayList<String>();
+                            while (start < 40) {
                                 DefaultHttpClient client = new DefaultHttpClient();
-                                HttpGet get = new HttpGet(String.format("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&start=%d", Uri.encode(searchText.getText().toString()), start));
+                                HttpGet get = new HttpGet(String.format("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&start=%d&imgsz=medium", Uri.encode(searchText.getText().toString()), start));
                                 HttpResponse resp = client.execute(get);
                                 HttpEntity entity = resp.getEntity();
                                 InputStream is = entity.getContent();
                                 final JSONObject json = new JSONObject(readToEnd(is));
                                 is.close();
                                 final JSONArray results = json.getJSONObject("responseData").getJSONArray("results");
-                                final ArrayList<String> urls = new ArrayList<String>();
                                 for (int i = 0; i < results.length(); i++) {
                                     JSONObject result = results.getJSONObject(i);
                                     urls.add(result.getString("url"));
                                 }
                                 
-                                // add the results to the adapter
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        for (String url: urls) {
-                                            mAdapter.add(url);
-                                        }
-                                    }
-                                });
-                                
-                                start += urls.size();
+                                start += results.length();
                             }
+                            // add the results to the adapter
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for (String url: urls) {
+                                        mAdapter.add(url);
+                                    }
+                                }
+                            });
                         }
                         catch (final Exception ex) {
                             // explodey error, lets toast it
